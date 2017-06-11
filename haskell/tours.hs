9e3fba57efb343ce3fc4e1ee67d3de1880b77c44
betrakiss <angelin.nedelchev@outlook.com>
2017-06-11 23:43:36 +0300

    Add tours potential solution.

---
+++ haskell/tours.hs
@@ -0,0 +1,10 @@
+type Trip = (String, Integer, Float)
+type Tour = [Trip]
+
+discount :: Tour -> Integer -> Tour
+discount tour len = map (apply len) tour
+
+apply :: Integer -> Trip -> Trip
+apply l (x, y, z)
+ | y > l = (x, y, z - z / 10)
+ | otherwise = (x, y, z)
