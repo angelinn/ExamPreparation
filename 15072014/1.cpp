6c017c12c7276be18b8171eae6302907fe36094d
betrakiss <angelin.nedelchev@outlook.com>
2017-06-12 22:10:16 +0300

    Add untested final solution.

--- 15072014/1.cpp
+++ 15072014/1.cpp
@@ -4,34 +4,76 @@ const int HEIGHT = 10;
 const int WIDTH = 10;
 const char PATH = '1';
 
+bool isAvailable(const char **, size_t, size_t);
+bool checkPattern(const char **, size_t, size_t, const char *);
+
 bool walk(const char** matrix, const char* pattern) 
 {
-    size_t patternSize = strlen(pattern);
-
-    size_t i, j = 0;
-    bool isOK = false;
-
-    for (size_t k = 0; k < patternSize; ++k) 
+    for (size_t i = 0; i < 9; ++i)
     {
-        switch (pattern[i]) 
+        if (matrix[i][0] == PATH)
+        {
+            if (checkPattern(matrix, i, 0, pattern))
+                return true;
+        }
+    }
+
+    for (size_t j = 0; j < 9; ++j)
+    {
+        if (matrix[0][j] == PATH)
+        {
+            if (checkPattern(matrix, 0, j, pattern))
+                return true;
+        }
+    }
+
+    for (size_t i = 9; i > 0; --i)
+    {
+        if (matrix[i][9] == PATH)
+        {
+            if (checkPattern(matrix, i, 9, pattern))
+                return true;
+        }
+    }
+
+    for (size_t j = 0; j > 0; --j)
+    {
+        if (matrix[9][j] == PATH)
+        {
+            if (checkPattern(matrix, 9, j, pattern))
+                return true;
+        }
+    }
+
+    return false;
+}
+
+bool checkPattern(const char** matrix, size_t i, size_t j, const char* pattern) 
+{
+    bool isOK = false;
+    while (*pattern)
+    {
+        switch (*pattern) 
         {
             case 'E':
-                isOK = isAvailable(matrix, i + 1, j);
+                isOK = isAvailable(matrix, ++i, j);
                 break;
             case 'W':
-                isOK = isAvailable(matrix, i - 1, j);
+                isOK = isAvailable(matrix, --i, j);
                 break;
             case 'N':
-                isOK = isAvailable(matrix, i, j - 1);
+                isOK = isAvailable(matrix, i, --j);
                 break;
             case 'S':
-                isOK = isAvailable(matrix, i, j + 1);
+                isOK = isAvailable(matrix, i, ++j);
                 break;
         }
 
         if (!isOK)
             break;
-    }
+
+        ++pattern;
+    }   
 
     return isOK;
 }
---
+++ 15072014/main.cpp
@@ -0,0 +1,6 @@
+#include "1.cpp"
+
+int main() 
+{
+    
+}
