9660a699d890441c916cce9dd3a66e593e5c16a3
betrakiss <angelin.nedelchev@outlook.com>
2017-06-13 21:28:17 +0300

    Add tests.

--- 14072015/main.cpp
+++ 14072015/main.cpp
@@ -1,4 +1,5 @@
 #include "1.cpp"
+#include "2.cpp"
 
 int main()
 {
@@ -7,4 +8,20 @@ int main()
     img[1] = new float[3] {4.5, 6.5, 7.5};
 
     subsample(2, 3, img);
+
+    Node* root = new Node { 8, new Node*[2], 2 };
+    root->children[0] = new Node { 2, nullptr, 0 };
+    root->children[1] = new Node { 3, new Node*[3], 3 };
+    
+    root->children[1]->children[0] = new Node { 4, nullptr, 0 };
+    root->children[1]->children[1] = new Node { 5, nullptr, 0 };
+    root->children[1]->children[2] = new Node { 6, nullptr, 0 };
+
+    printf("Is 3 member of 8: %d\n", member(root, 3));
+    printf("Is 4 member of 3: %d\n", member(root->children[1], 4));
+    printf("Is 2 member of 3: %d\n", member(root->children[1], 2));
+
+    filterOdd(root);
+
+    deleteNode(root);
 }
