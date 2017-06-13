e3576edfc01c2167aff536a54b8e16275c341bb4
betrakiss <angelin.nedelchev@outlook.com>
2017-06-13 23:05:17 +0300

    Add lambda filtering even numbers.

--- 10092015/3.hs
+++ 10092015/3.hs
@@ -4,3 +4,5 @@ merge [] y = y
 merge (x:xs) (y:ys) = if x < y
                         then x : merge xs (y:ys)
                         else y : merge (x:xs) ys
+
+(\list -> [y | y <- list, even y]) [1, 2, 3, 4]
