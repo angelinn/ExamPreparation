69cd66d7f4834b970021722844f7643c6938693b
betrakiss <angelin.nedelchev@outlook.com>
2017-06-13 17:28:41 +0300

    Add case or 0 and last case.

--- 15072014/3.hs
+++ 15072014/3.hs
@@ -7,15 +7,16 @@ totalMinRec (x:xs) min foo
     | min < x 0 = totalMinRec xs min foo
     | otherwise = totalMinRec xs (x 0) x
 
-chainMinCompositions :: (Int -> Int) -> [(Int -> Int)]
+chainMinCompositions :: (Int -> Int) -> [Int -> Int]
 chainMinCompositions f = chainMinRec f [] 
 
 chainMinRec :: (Int -> Int) -> [Int -> Int] -> [Int -> Int]
 chainMinRec f foos
-    | len < 1                                     = f : chainMinRec f (foos ++ [f])
+    | len == 0                                    = chainMinRec id (foos ++ [id])
+    | len == 1                                    = f : chainMinRec f (foos ++ [f])
     | or [ preprev i /= prev i  | i <- [1..len] ] = let foo = (prev . preprev)
                                                     in f : chainMinRec foo (foos ++ [foo])
-    | otherwise                                   = let foo = totalMin foos
+    | and [ preprev i == prev i | i <- [1..len] ] = let foo = totalMin foos
                                                     in f : chainMinRec foo (foos ++ [foo])
     where 
         preprev = last (init foos)
