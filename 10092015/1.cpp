30231c3b1396799a036cb50c3656f334c0838d1b
betrakiss <angelin.nedelchev@outlook.com>
2017-06-13 22:39:43 +0300

    Add selection sort.

--- 10092015/1.cpp
+++ 10092015/1.cpp
@@ -7,3 +7,24 @@ bool member(int x, int a[], int size)
           (a[size / 2] < x && member(x, a + size / 2, size / 2)) || 
           (member(x, a, size / 2));
 }
+
+void selectionSort(int* nums, int N)
+{
+    for (int i = 0; i < N; ++i) 
+    {
+        int min = i;
+
+        for (int j = i + 1; j < N; ++j) 
+        {
+            if (nums[j] < nums[min])
+                min = j;
+        }
+
+        if (min != i) 
+        {
+            int x = nums[i];
+            nums[i] = nums[min];
+            nums[min] = x;
+        }
+    }
+}
--- 10092015/main.cpp
+++ 10092015/main.cpp
@@ -5,4 +5,14 @@ int main()
 {
     int a[] = { 1, 2, 3, 4 ,5 ,6 ,7 ,8 ,9 ,10 };
     printf("Is 9 a member : %d\n", member(9, a, 10));
+
+
+    int b[] = { 10, 14, 7, 0, 23, 1, 11 };
+    int N = 7;
+    selectionSort(b, N);
+
+    for (int i = 0; i < N; ++i)
+        printf ("%d, ", b[i]);
+    
+    printf("\n");
 }
