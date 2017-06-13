5eb1a03089a62ce6bdc6af41cd10d7ae6bd53c29
betrakiss <angelin.nedelchev@outlook.com>
2017-06-13 17:18:25 +0300

    Add potential solution for task three.

--- 15072014/3.hs
+++ 15072014/3.hs
@@ -1,8 +1,23 @@
-totalMin :: [(Int -> Int)] -> (Int -> Int)
+totalMin :: [Int -> Int] -> (Int -> Int)
 totalMin (x:xs) = (totalMinRec xs (x 0) x)
 
-totalMinRec :: [(Int -> Int)] -> Int -> (Int -> Int) -> (Int -> Int)
+totalMinRec :: [Int -> Int] -> Int -> (Int -> Int) -> (Int -> Int)
 totalMinRec [] _ foo = foo
 totalMinRec (x:xs) min foo
     | min < x 0 = totalMinRec xs min foo
     | otherwise = totalMinRec xs (x 0) x
+
+chainMinCompositions :: (Int -> Int) -> [(Int -> Int)]
+chainMinCompositions f = chainMinRec f [] 
+
+chainMinRec :: (Int -> Int) -> [Int -> Int] -> [Int -> Int]
+chainMinRec f foos
+    | len < 1                                     = f : chainMinRec f (foos ++ [f])
+    | or [ preprev i /= prev i  | i <- [1..len] ] = let foo = (prev . preprev)
+                                                    in f : chainMinRec foo (foos ++ [foo])
+    | otherwise                                   = let foo = totalMin foos
+                                                    in f : chainMinRec foo (foos ++ [foo])
+    where 
+        preprev = last (init foos)
+        prev    = last foos
+        len     = length foos
