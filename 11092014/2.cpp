ddb287c307d5dc1611724ef56a1231be326e7be9
betrakiss <angelin.nedelchev@outlook.com>
2017-06-14 23:21:37 +0300

    Fix SeaMonster movement.

--- 11092014/2.cpp
+++ 11092014/2.cpp
@@ -1,164 +1,201 @@
 #include <cstdio>
 #include <cctype>
 
-extern int** field;
-extern int N;
+extern char** field;
+extern const int N;
 
-const int LAND = 0;
-const int FIRE = 1;
-const int WATER = 2;
+const char LAND = '0';
+const char FIRE = '1';
+const char WATER = '2';
 
 struct Point
 {
-    int x;
-    int y;
+	int x;
+	int y;
+
+	Point(int newX = -1, int newY = -1) : x(newX), y(newY)
+	{   }
 };
 
 bool isValidPosition(int x, int y)
 {
-    return (x >= 0 && y >= 0 && x < N && y < N);
+	return (x >= 0 && y >= 0 && x < N && y < N);
 }
 
 Point* getNearbyPositions(const Point& point)
 {
-    Point* positions = new Point[4] { { 0, 0 } };
-    size_t size = 0;
+	Point* positions = new Point[N];
+	size_t size = 0;
 
-    if (isValidPosition(point.x, point.y + 1))
-        positions[size++] = Point { point.x, point.y + 1 };
-    
-    if (isValidPosition(point.x, point.y - 1))
-        positions[size++] = Point { point.x, point.y - 1 };
+	if (isValidPosition(point.x, point.y + 1))
+		positions[size++] = Point{ point.x, point.y + 1 };
 
-    if (isValidPosition(point.x + 1, point.y))
-        positions[size++] = Point { point.x + 1, point.y };
+	if (isValidPosition(point.x, point.y - 1))
+		positions[size++] = Point{ point.x, point.y - 1 };
 
-    if (isValidPosition(point.x - 1, point.y))
-        positions[size++] = Point { point.x - 1, point.y }; 
+	if (isValidPosition(point.x + 1, point.y))
+		positions[size++] = Point{ point.x + 1, point.y };
 
-    return positions;
+	if (isValidPosition(point.x - 1, point.y))
+		positions[size++] = Point{ point.x - 1, point.y };
+
+	return positions;
 }
 
 class GamePlayer
 {
 public:
-    virtual Point getPosition()
-    {
-        return currentPosition;
-    }
+	virtual Point getPosition()
+	{
+		return currentPosition;
+	}
 
-    virtual Point* allowedMoves() = 0;
-    virtual const char* getTag() = 0;
+	virtual Point* allowedMoves() = 0;
+	virtual const char* getTag() = 0;
 
 protected:
-    Point currentPosition;
+	Point currentPosition;
 };
 
 class Knight : public GamePlayer
 {
 public:
-    virtual Point* allowedMoves()
-    {
-        Point* moves = new Point[4] { {0, 0} };
-        size_t currentMove = 0;
+	virtual Point* allowedMoves()
+	{
+		Point* moves = new Point[N];
 
-        Point* nearPositions = getNearbyPositions(currentPosition);
-        for (int i = 1; i && i < 5; ++i)
-        {
-            if (!hasAdjacentFire(nearPositions[i].x, nearPositions[i].y))
-                moves[currentMove++] = nearPositions[i];
-        }
+		size_t currentMove = 0;
+		Point* nearPositions = getNearbyPositions(currentPosition);
+		for (int i = 0; i < N; ++i)
+		{
+			if (nearPositions[i].x == -1)
+				break;
 
-        delete[] nearPositions;
-        return moves;
-    }
+			if (!hasAdjacentFire(nearPositions[i].x, nearPositions[i].y))
+				moves[currentMove++] = nearPositions[i];
+		}
 
-    virtual const char* getTag() { return "Knight"; }
+		delete[] nearPositions;
+		return moves;
+	}
+
+	virtual const char* getTag() { return "Knight"; }
 
 private:
-    bool hasAdjacentFire(int x, int y)
-    {
-        if (isValidPosition(x, y + 1))
-        {
-            if (field[x][y + 1] == FIRE)
-                return true;
-        }
+	bool hasAdjacentFire(int x, int y)
+	{
+		if (isValidPosition(x, y + 1))
+		{
+			if (field[x][y + 1] == FIRE)
+				return true;
+		}
 
-        if (isValidPosition(x, y - 1))
-        {
-            if (field[x][y - 1] == FIRE)
-                return true;
-        }
+		if (isValidPosition(x, y - 1))
+		{
+			if (field[x][y - 1] == FIRE)
+				return true;
+		}
 
-        if (isValidPosition(x + 1, y))
-        {
-            if (field[x + 1][y] == FIRE)
-                return true;
-        }
+		if (isValidPosition(x + 1, y))
+		{
+			if (field[x + 1][y] == FIRE)
+				return true;
+		}
 
-        if (isValidPosition(x - 1, y))
-        {
-            if (field[x - 1][y] == FIRE)
-                return true;
-        }
+		if (isValidPosition(x - 1, y))
+		{
+			if (field[x - 1][y] == FIRE)
+				return true;
+		}
 
-        return false;
-    }
+		return false;
+	}
 };
 
 class SeaMonster : public GamePlayer
 {
 public:
-    virtual Point* allowedMoves()
-    {
-        Point* moves = new Point[4] { {0, 0} };
-        size_t currentMove = 0;
-        
-        Point* nearPositions = getNearbyPositions(currentPosition);
-        for (int i = 1; i && i < 5; ++i)
-        {
-            if (field[nearPositions[i].x][nearPositions[i].y] == WATER)   
-                moves[currentMove++] = Point { currentPosition.x, currentPosition.y + 1};
-        }
-        delete[] nearPositions;
+	SeaMonster(Point point)
+	{
+		currentPosition = point;
+	}
 
-        return moves;
-    }
+public:
+	virtual Point* allowedMoves()
+	{
+        return allowedMovesRecursive(currentPosition);
+	}
 
-    virtual const char* getTag() { return "SeaMonster"; }
+	virtual const char* getTag() { return "SeaMonster"; }
 
 private:
-    bool isValidPosition(int x, int y)
+    Point* allowedMovesRecursive(const Point& start)
     {
-        return (x >= 0 && y >= 0 && x < N && y < N) && field[x][y] == WATER;
+		Point* moves = new Point[N];
+		size_t currentMove = 0;
+
+		Point* nearPositions = getNearbyPositions(start);
+        for (int i = currentPosition.x - 1; i >= 0; --i)
+        {
+            if (field[i][currentPosition.y] != WATER)
+                break;
+
+            moves[currentMove++] = Point(i, currentPosition.y);
+        }
+
+        for (int i = currentPosition.x + 1; i < N; ++i)
+        {            
+            if (field[i][currentPosition.y] != WATER)
+                break;
+            moves[currentMove++] = Point(i, currentPosition.y);
+        }
+
+        for (int i = currentPosition.y - 1; i >= 0; --i)
+        {
+            if (field[currentPosition.x][i] != WATER)
+                break;
+            moves[currentMove++] = Point(currentPosition.x, i);
+        }
+
+        for (int i = currentPosition.y + 1; i < N; ++i)
+        {
+            if (field[currentPosition.x][i] != WATER)
+                break;
+            moves[currentMove++] = Point(currentPosition.x, i);
+        }
+
+		return moves;
     }
+
+	bool isValidPosition(int x, int y)
+	{
+		return (x >= 0 && y >= 0 && x < N && y < N) && field[x][y] == WATER;
+	}
 };
-
-bool isAtWar(int x, int y)
-{
-    if (!isValidPosition(x, y))
-        return false;
-
-    return isalpha(field[x][y]);
-}
 
 void allMoves(GamePlayer** players, size_t size)
 {
-    for (size_t i = 0; i < size; ++i)
-    {
-        Point* allowedMoves = players[i]->allowedMoves();
-        for (int i = 1; i && i < 5; ++i)
-        {
-            Point* nearPositions = getNearbyPositions(players[i]->getPosition());
-            for (int j = 1; j && j < 5; ++j)
-            {
-                if (isalpha(field[nearPositions[j].x][nearPositions[j].y]))
-                    printf("%s (%d, %d) -> (%d, %d)\n", players[i]->getTag(), players[i]->getPosition().x, players[i]->getPosition().y, nearPositions[j].x, nearPositions[j].y);
-            }
+	for (size_t i = 0; i < size; ++i)
+	{
+		Point* allowedMoves = players[i]->allowedMoves();
+		for (int i = 0; i < N; ++i)
+		{
+			if (allowedMoves[i].x == -1)
+				break;
 
-            ++allowedMoves;
-            delete[] nearPositions;
-        }
-        delete[] allowedMoves;
-    }
+			Point* nearPositions = getNearbyPositions(players[i]->getPosition());
+			for (int j = 0; j < N; ++j)
+			{
+				if (nearPositions[j].x == -1)
+					break;
+
+				if (isalpha(field[nearPositions[j].x][nearPositions[j].y]))
+					printf("%s (%d, %d) -> (%d, %d)\n", players[i]->getTag(), players[i]->getPosition().x, players[i]->getPosition().y, nearPositions[j].x, nearPositions[j].y);
+			}
+
+			++allowedMoves;
+			delete[] nearPositions;
+		}
+		delete[] allowedMoves;
+	}
 }
