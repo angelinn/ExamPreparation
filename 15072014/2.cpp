c7d38a356a9da8320f875d75d1a071674f450046
betrakiss <angelin.nedelchev@outlook.com>
2017-06-16 15:18:26 +0300

    Implement Rook and Knight.

--- 15072014/2.cpp
+++ 15072014/2.cpp
@@ -1,25 +1,102 @@
 struct ChessPosition
 {
-    char x;
-    int y;
+    int x;
+    char y;
+
+    ChessPosition(int x = -1, char y = -1) : x(x), y(y)
+    {   }
 };
 
 class ChessPiece
 {
 public:
-    virtual ChessPosition getPosition() = 0;
     virtual ChessPosition* allowedMoves() = 0;
-    virtual bool captures(ChessPosition pos) = 0;
+    ChessPosition getPosition() { return position; }
+    bool captures(ChessPosition pos)
+    {
+        ChessPosition* moves = allowedMoves();
+        for (int i = 0; moves[i].x != -1; ++i)
+        {
+            if (moves[i] == pos)
+            {
+                delete[] moves;
+                return true;
+            }
+        }
+
+        delete[] moves;
+        return false;
+    }
+
+protected:
+    ChessPosition position;
 };
 
 class Rook : public ChessPiece
 {
+public:
+    virtual ChessPosition* allowedMoves()
+    {
+        size_t moveCount = 0;
+        ChessPosition* moves = new ChessPosition[16];
+        for (char i = position.y; i <= 'H'; ++i)
+            moves[moveCount++] = ChessPosition { position.x, i };
+            
+        for (char i = position.y; i >= 'A'; --i)
+            moves[moveCount++] = ChessPosition { position.x, i };
 
+        for (int i = position.x; i <= 8; ++i)
+            moves[moveCount++] = ChessPosition { i, position.y };
+            
+        for (int i = position.x; i >= 1; --i)
+            moves[moveCount++] = ChessPosition { i, position.y };
+
+        return moves;
+    }
 };
 
 class Knight : public ChessPiece
 {
+public:
+    virtual ChessPosition* allowedMoves()
+    {
+        size_t moveCount = 0;
+        ChessPosition* moves = new ChessPosition[8];
 
+        if (position.x + 3 <= 8)
+        {
+            if (position.y + 1 <= 'H')
+                moves[moveCount++] = ChessPosition { position.x + 3, position.y + 1 };
+            if (position.y - 1 >= 'A')
+                moves[moveCount++] = ChessPosition { position.x + 3, position.y - 1 };
+        }
+
+        if (position.x - 3 >= 1)
+        {
+            if (position.y + 1 <= 'H')
+                moves[moveCount++] = ChessPosition { position.x - 3, position.y + 1 };
+            if (position.y - 1 >= 'A')
+                moves[moveCount++] = ChessPosition { position.x - 3, position.y - 1 };
+        }
+
+        if (position.y + 3 <= 'H')
+        {
+            if (position.x + 1 <= 8)
+                moves[moveCount++] = ChessPosition { position.x + 1, position.y + 3 };
+            if (position.x - 1 >= 1)
+                moves[moveCount++] = ChessPosition { position.x - 1, position.y + 3 };
+        }
+
+        if (position.y - 3 >= 'A')
+        {
+            if (position.x + 1 <= 8)
+                moves[moveCount++] = ChessPosition { position.x + 1, position.y - 3 };
+            if (position.x - 1 >= 1)
+                moves[moveCount++] = ChessPosition { position.x - 1, position.y - 3 };
+        }
+
+        return moves;
+    }
 };
 
 void allMoves(ChessPiece** pieces) 
