0963198206707aff8904840ec25798f3879d2d68
betrakiss <angelin.nedelchev@outlook.com>
2017-06-22 16:15:55 +0300

    Add potential solution for graph task.

---
+++ 10092015/2.cpp
@@ -0,0 +1,86 @@
+#include <cstdio>
+
+bool DFS(int** g, int i, bool* visited, size_t N)
+{
+	bool notVisited = false;
+	if (!visited[i])
+		notVisited = true;
+
+	visited[i] = true;
+
+	for (int j = 0; j < N; ++j)
+	{
+		if (!visited[j] && g[i][j])
+		{
+			DFS(g, j, visited, N);
+		}
+	}
+
+	return notVisited;
+}
+
+int** reverseGraph(int** g, size_t N)
+{
+	int** rg = new int*[N];
+	for (size_t i = 0; i < N; ++i)
+		rg[i] = new int[N];
+
+	bool** visited = new bool*[N];
+    for (size_t i = 0; i < N; ++i)
+        visited[i] = new bool[N] { false };
+
+	for (size_t i = 0; i < N; ++i)
+	{
+		for (size_t j = 0; j < N; ++j)
+		{
+			if (g[i][j] && !visited[i][j])
+			{
+                visited[i][j] = true;
+                visited[j][j] = true;
+
+				rg[j][i] = g[i][j];
+				g[i][j] = 0;
+			}
+		}
+	}
+
+    for (size_t i = 0; i < N; ++I)
+        delete[] visited[i];
+        
+	delete[] visited;
+	return rg;
+}
+
+bool isConnected(int** g, size_t N, bool reversed = false)
+{
+	bool connected = true;
+
+	bool* visited = new bool[N] { false };
+	DFS(g, 0, visited, N);
+	for (size_t i = 1; i < N; ++i)
+	{
+		if (DFS(g, i, visited, N))
+			connected = false;
+	}
+
+	delete[] visited;
+
+	if (connected)
+		return true;
+
+	if (reversed)
+		return connected;
+
+	int** newGraph = reverseGraph(g, N);
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N; ++j)
+            printf("[%d][%d] = %d\n", i, j, newGraph[i][j]);
+        printf("\n");
+    }
+        
+	if (!isConnected(newGraph, N, true))
+		return false;
+
+	return true;
+}
--- 10092015/main.cpp
+++ 10092015/main.cpp
@@ -1,20 +1,29 @@
 #include "1.cpp"
+#include "2.cpp"
 #include <cstdio>
 
 int main()
 {
-    int a[] = { 1, 2, 3, 4 ,5 ,6 ,7 ,8 ,9 ,10 };
-    printf("Is 9 a member : %d\n", member(9, a, 10));
+    // int a[] = { 1, 2, 3, 4 ,5 ,6 ,7 ,8 ,9 ,10 };
+    // printf("Is 9 a member : %d\n", member(9, a, 10));
 
 
-    int b[] = { 10, 14, 7, 0, 23, 1, 11 };
-    int N = 7;
-    selectionSort(b, N);
+    // int b[] = { 10, 14, 7, 0, 23, 1, 11 };
+    // int N = 7;
+    // selectionSort(b, N);
 
-    for (int i = 0; i < N; ++i)
-        printf ("%d, ", b[i]);
+    // for (int i = 0; i < N; ++i)
+    //     printf ("%d, ", b[i]);
     
-    printf("\n");
+    // printf("\n");
 
-    printf("%d\n", somethingElse());
+    // printf("%d\n", somethingElse());
+
+    int** g = new int*[4];
+    g[0] = new int[4] { 0, 1, 0, 0 };
+    g[1] = new int[4] { 0, 0, 0, 0 };
+    g[2] = new int[4] { 0, 1, 0, 0 };
+    g[3] = new int[4] { 0, 0, 1, 0 };
+
+    printf("Connected: %d\n", isConnected(g, 4));
 }
